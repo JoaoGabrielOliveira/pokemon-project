0f1eb94c604394e5ea7ad385308b4593663cd656
JoaoGabrielOliveira <joaogabrieloliveira81@gmail.com>
2020-05-13 14:06:46 -0300

    [ Back-end ] Correções de Cadastro e criando perfil João e Bruna

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+joao = User.new
+joao.first_name = 'João'
+joao.last_name = 'G.'
+joao.email = 'joao@gmail.com'
+joao.date_of_birth = Date.new
+joao.password = '123456'
+joao.sex = 'm'
+
+bruna = User.new
+bruna.first_name = 'Bruna'
+bruna.last_name = 'C.'
+bruna.email = 'bruna@gmail.com'
+bruna.date_of_birth = Date.new
+bruna.password = '123456'
+bruna.sex = 'f'
