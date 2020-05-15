7e0aaf480472747620a4c042e3529cacffb20802
JoaoGabrielOliveira <joaogabrieloliveira81@gmail.com>
2020-05-15 16:09:03 -0300

    [ Back-end ] Adicioando paramentro de limite

--- app/controllers/api/v1/pokemon_controller.rb
+++ app/controllers/api/v1/pokemon_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::PokemonController < ApplicationController
     def index
-        @pokemons = Pokemon.all
+        if (params[:limit].to_i > 0)
+            @pokemons = Pokemon.take(params[:limit].to_i)
+        else 
+            @pokemons = Pokemon.all
+        end
     end
 
     def show
