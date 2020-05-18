751da4d51fb11da0f19422dcc4446ef53d079c14
JoaoGabrielOliveira <joaogabrieloliveira81@gmail.com>
2020-05-18 15:19:50 -0300

    [ Back-end ] Criando filtro order de listagem

--- app/controllers/api/v1/pokemon_controller.rb
+++ app/controllers/api/v1/pokemon_controller.rb
@@ -1,15 +1,20 @@
 class Api::V1::PokemonController < ApplicationController
     def index
-        if (params[:limit].to_i > 0)
+        if params[:limit].to_i > 0
             @pokemons = Pokemon.take(params[:limit].to_i)
         else 
             @pokemons = Pokemon.all
         end
+
+        if params[:order] != ''
+            @pokemons = @pokemons.sort_by {|e| e[params[:order]]}
+        end
+
     end
 
     def show
         begin
-            @pokemon = Pokemon.find(params[:id])    
+            @pokemon = Pokemon.find(params[:id])
         rescue => exception
         end
     end
