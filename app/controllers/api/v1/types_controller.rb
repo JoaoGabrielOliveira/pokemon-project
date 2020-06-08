b47042f5d8b00348abe43fc35a5a1a26dec392c6
= <=>
2020-06-08 09:54:03 -0300

    [back-end] Adicionado filtros de pesquisa em Types

--- app/controllers/api/v1/types_controller.rb
+++ app/controllers/api/v1/types_controller.rb
@@ -1,9 +1,26 @@
 class Api::V1::TypesController < ApplicationController
   def index
-    @types = PokemonType.all 
-  end
+    if params[:offset].to_i > 0
+        @types = PokemonType.all.drop(params[:offset].to_i)
+    else 
+        @types = PokemonType.all
+    end
 
-  def show
-    @type = PokemonType.find(params[:id])
-  end
+
+    if params[:limit].to_i > 0
+        @types = @types.take(params[:limit].to_i)
+    end
+
+    if params[:order] != ''
+        @types = @types.sort_by {|e| e[params[:order]]}
+    end
+
+end
+
+def show
+    begin
+        @type = PokemonType.find(params[:id])
+    rescue => exception
+    end
+end
 end
