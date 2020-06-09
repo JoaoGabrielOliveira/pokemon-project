73630ea384a2e750c0a095258046eb773d102285
= <=>
2020-06-09 13:34:50 -0300

    [back-end]Mudando estrutura do projeto

--- app/javascript/paginas/pokemon/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TelaPokemon = () => {
-    return(
-        <h1> Tela Pokemon </h1>
-    )
-}
-
-export default TelaPokemon;
--- app/javascript/paginas/principal/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TelaPrincipal = () => {
-    return(
-        <h1> Tela Principal </h1>
-    )
-}
-
-export default TelaPrincipal;
