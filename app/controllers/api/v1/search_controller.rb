23dd778f54c39e1bd0fd0b6ed8f19e46e75d2114
= <=>
2020-07-03 16:03:10 -0300

    [Back-end] Selecionar por tipo

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -9,19 +9,30 @@ class Api::V1::SearchController < ApplicationController
         end
             
         if params[:show] == 'pokemon' || params[:show] == 'p'
-            @queryPokemons = Pokemon.order(:name).where('name LIKE ?', "#{params[:q]}%")
-
+            @queryPokemons = Pokemon.order(:id).where('name LIKE ?', "#{params[:q]}%")
+        
         elsif params[:show] == 'type' || params[:show] == 't'
             @queryTypes = PokemonType.where('name LIKE ?', "#{params[:q]}%")
 
         elsif params[:bytype].to_i > 0
-            pokemon_ids = PokemonToType.where('pokemon_type_1 = ? or pokemon_type_2 = ?', "#{params[:bytype].to_i}", "#{params[:bytyoe].to_i}").map(& :pokemon_id)
+            pokemon_ids = PokemonToType.order(:pokemon_id).where('pokemon_type_1 = ? or pokemon_type_2 = ?', "#{params[:bytype].to_i}", "#{params[:bytyoe].to_i}").map(& :pokemon_id)
+            @queryPokemons = []
+
+            pokemon_ids.each do |id|
+                @queryPokemons.push(Pokemon.find(id))
+            end
+            
+            @queryPokemons.sort_by {|e| e[params[:name]]}
+
+        elsif params[:byegg].to_i > 0
+            pokemon_ids = PokemonToEggGroup.order(:pokemon_id).where('egg_group_id = ?', "#{params[:byegg].to_i}").map(& :pokemon_id)
             @queryPokemons = []
 
             pokemon_ids.each do |id|
                 @queryPokemons.push(Pokemon.find(id))
             end
 
+            @queryPokemons.sort_by {|e| e[:name]}
         else
             @queryTypes = PokemonType.where('name LIKE ?', "#{params[:q]}%")
             @queryPokemons = Pokemon.where('name LIKE ?', "#{params[:q]}%")
