1e1c472f42ebe9fcf075b4de8b4676abf43691a9
= <=>
2020-07-13 12:10:16 -0300

    [Bug] Correções de caminho

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,8 @@ import  './App.css';
 import Home from './Home/Home';
 import Dashboard from './component/layout/Dashboard';
 import Pokemon from './component/layout/pokemon/Pokemon';
-import Noticia from './Page/Noticia';
+
+import Noticia from './Noticia/index';
 
 
 class App extends Component {
@@ -17,7 +18,7 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Switch>
-              
+              <Route exact path="/" component={Home} />
               <Route exact path="/pokemons" component={Dashboard} />
               <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />
               <Route exact path="/noticia" component={Noticia} />
