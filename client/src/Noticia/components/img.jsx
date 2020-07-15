46adf92db870f8a86cf5b2bf833c76a82ca46c3e
= <=>
2020-07-15 14:47:31 -0300

    [Front-end]Criando função na API e integrando na Pagina de Noticias

--- client/src/Noticia/components/img.jsx
+++ client/src/Noticia/components/img.jsx
@@ -3,7 +3,7 @@ import React from  'react';
 
 export default props =>
     <div>
-            <img src={props.src} alt="ola" />
+            <img src={props.src} alt="ola" style={{maxWidth:'100%', borderRadius:'0px'}} />
         <div>
             {props.children}
         </div>
