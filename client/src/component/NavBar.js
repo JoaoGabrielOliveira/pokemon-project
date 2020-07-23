05e04d13dfb0e2961abbffb17237ab03938ba549
= <=>
2020-07-23 16:06:36 -0300

    [Bug]

--- client/src/component/NavBar.js
+++ client/src/component/NavBar.js
@@ -3,16 +3,12 @@ import React, { Component } from 'react';
 export default class NavBar extends Component {
     render() {
         let imagem = `https://www.apple-tribe.com/wp-content/uploads/2016/07/pokemon-pikachu-go-2.jpg`;
-
         let strongText = <strong style={{marginLeft:''}}>Pokedex</strong>;
-         let   strong = <strong style={{marginLeft:''}}>Noticia</strong>;
-
-        //let imagem = `https://img.icons8.com/officel/80/000000/pokedex.png`
         let links = [
             <a href="/">Início</a>,
-            <a href="/Pokemons">Pokedex</a>,
-            <a href="/Noticia">Notícia</a>,
-            <a href="/Contato">Contato</a>
+            <a href="/pokemons">Pokedex</a>,
+            <a href="/noticia">Notícia</a>,
+            <a href="/contato">Contato</a>
         ];
 
         if (this.props.image != undefined )
@@ -20,8 +16,8 @@ export default class NavBar extends Component {
 
         if (this.props.text == true)
             strongText = undefined; 
-            else if(this.props.text === false)
-             strong=this.props.strong
+            else if(this.props.text != '')
+            strongText = <strong style={{marginLeft:''}}>{this.props.text}</strong>;
              
              return (
                 <nav className="navbar">
--- client/src/paginas/Noticia/index.js
+++ client/src/paginas/Noticia/index.js
@@ -8,7 +8,7 @@ import './index.css';
 
 export default () => (
     <>
-        <NavBar image='https://cdn.bulbagarden.net/upload/thumb/1/1e/Detective_Pikachu_artwork_3.png/153px-Detective_Pikachu_artwork_3.png' text="Noticia"/>
+        <NavBar image='https://cdn.bulbagarden.net/upload/thumb/1/1e/Detective_Pikachu_artwork_3.png/153px-Detective_Pikachu_artwork_3.png' text="Notícia"/>
         <Layout />
         <Dina/>
 
